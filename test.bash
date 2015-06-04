62917705ae5bd97e03401e58f2989ce222cb9c17
Shuhei YAMAMOTO <a1548sy@aiit.ac.jp>
2015-06-05 00:24:50 +0900

    Second Commit 20150604

--- test.bash
+++ test.bash
@@ -1,5 +1,29 @@
 #!/bin/bash
+tmp=/tmp/$$
+ERROR_EXIT () {
+  echo "$1" >&2
+  rm -f $tmp-*
+  exit 1
+}
+#####################################
+# TEST1
+#####################################
 
-echo "This is a Test."
-echo "AAAAA"
-echo "BBBBB"
+echo 4 > $tmp-ans
+./15745148 12 8 > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+
+rm -f $tmp-*
+
+#####################################
+# TEST2
+#####################################
+
+echo 5 > $tmp-ans
+./15745148 10 5 > $tmp-out || ERROR_EXIT "TEST2-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST2-2"
+
+echo OK
+
+exit 0
+
